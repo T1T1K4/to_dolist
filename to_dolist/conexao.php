0ee94a42106188a21439a80e93863232b3e047c1
Carlos Eduardo <151068498+T1T1K4@users.noreply.github.com>
2024-11-08 19:51:46 -0300

    Delete to_dolist directory

--- to_dolist/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost'; #127.0.0.1
-$usuario = 'root';
-$senha = '';
-$banco = 'to_dolist';
-
-$conn = mysqli_connect($host, $usuario, $senha, $banco) or die('Não foi possível conectar');
-
-?>
